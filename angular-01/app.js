9548c9a9d035873d503b18c8710be75a649abf2d
Tetiana Khotiaintseva <tetiana.khot@gmail.com>
2017-02-18 21:59:11 +0100

    Name calculator app

--- angular-01/app.js
+++ angular-01/app.js
@@ -1,12 +1,22 @@
 (function (){
   'use strict';
 
-  angular.module('helloWorldApp', [])
-  .controller('myFirstController', function ($scope){
-    $scope.name = "Tetiana";
-    $scope.sayHello = function() {
-      return "Hello there";
-    };
+  angular.module('NameCalculator', [])
+
+  .controller('NameCalculatorController', function ($scope){
+    $scope.name = "";
+    $scope.totalValue = 0;
+    $scope.displayNumeric = function() {
+      $scope.totalValue = calculateNumericForString($scope.name);
+    }
   });
+
+function calculateNumericForString(string) {
+  var totalStringValue = 0;
+  for (var i = 0; i < string.length; i ++) {
+    totalStringValue += string.charCodeAt(i);
+  }
+  return totalStringValue;
+}
 
 })();
